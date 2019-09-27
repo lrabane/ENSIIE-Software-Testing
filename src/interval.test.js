b374039b73b83c21f89e54e766335e4d7f6897fd
lrabane <lucas@rabane.dev>
2019-09-27 10:56:12 +0200

    Refactor interval test

--- src/interval.test.js
+++ src/interval.test.js
@@ -4,32 +4,40 @@ var interval1 = new Interval(1, 4);
 var interval2 = new Interval(5, 9);
 var interval3 = new Interval(3, 11);
 
-describe('overlaps', function () {
-    test('interval1 doesn\' overlap interval2', () => {
-        expect(interval1.overlaps(interval2)).toBe(false);
-    });
 
-    test('interval1 overlap interval3', () => {
-        expect(interval1.overlaps(interval3)).toBe(true);
-    });
+describe('overlaps', function () {
+    test.each([
+        [new Interval(1, 4), new Interval(5, 9), false],
+        [new Interval(1, 4), new Interval(3, 11), true],
+    ])(
+        '%s overlaps %s equals %s',
+        (interval1, interval2, expected) => {
+            expect(interval1.overlaps(interval2)).toBe(expected);
+        }
+    );
 });
 
-describe('uncludes', function () {
-    test('interval1 doesn\' include interval2', () => {
-        expect(interval1.overlaps(interval2)).toBe(false);
-    });
 
-    test('interval3 includes interval2', () => {
-        expect(interval3.overlaps(interval2)).toBe(true);
-    });
+describe('includes', function () {
+    test.each([
+        [new Interval(1, 4), new Interval(5, 9), false],
+        [new Interval(3, 11), new Interval(5, 9), true],
+    ])(
+        '%s includes %s equals %s',
+        (interval1, interval2, expected) => {
+            expect(interval1.includes(interval2)).toBe(expected);
+        }
+    );
 });
 
 describe('union', function () {
-    test('interval1 union interval2', () => {
-        expect(interval1.union(interval2)).toStrictEqual([interval1, interval2]);
-    });
-
-    test('interval3 union interval2', () => {
-        expect(interval3.union(interval2)).toStrictEqual([interval3]);
-    });
+    test.each([
+        [new Interval(1, 4), new Interval(5, 9), [new Interval(1, 4), new Interval(5, 9)]],
+        [new Interval(3, 11), new Interval(5, 9), [interval3]],
+    ])(
+        '%s union %s equals %s',
+        (interval1, interval2, expected) => {
+            expect(interval1.union(interval2)).toStrictEqual(expected);
+        }
+    );
 });
